56c8be9f210ecf1b5e5a8da47a0d72c8eec5fbc3
binnbinny <2300030843@kluniversity.in>
2026-05-03 20:36:40 +0530

    Time: 2 ms (86.99%), Space: 47.6 MB (90.47%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int MaxReach = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            if(i > MaxReach){
+                return false;
+            }
+            MaxReach = Math.max(MaxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
