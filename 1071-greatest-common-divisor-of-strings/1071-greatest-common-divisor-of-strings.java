ccf899b50a529b25a1fcc6988b17de76ba98cc47
binnbinny <2300030843@kluniversity.in>
2026-06-19 12:10:46 +0530

    Time: 1 ms (94.36%), Space: 43.8 MB (26.92%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+
+        }
+        int len1 = str1.length();
+        int len2 = str2.length();
+        while(len2 != 0){
+            int temp = len2;
+            len2 = len1%len2;
+            len1 = temp;
+        }
+        return str1.substring(0,len1);
+    }
+}
