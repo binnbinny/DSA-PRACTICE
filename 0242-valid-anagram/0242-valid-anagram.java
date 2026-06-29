a34381f0d3578c98c2b166c250b8a932ec5d367f
binnbinny <2300030843@kluniversity.in>
2026-06-29 23:01:07 +0530

    Time: 18 ms (13.64%), Space: 46.5 MB (33.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i = 0; i < s.length(); i++){
+            char current = s.charAt(i);
+            if(map.containsKey(current)){
+                int oldCount = map.get(current);
+                int newCount = oldCount + 1;
+                map.put(current,newCount);
+            }
+            else{
+                map.put(current, 1);
+            }
+        }
+        for(int j = 0; j < t.length(); j++){
+            char current = t.charAt(j);
+            if(map.containsKey(current)){
+                int oldCount  =  map.get(current);
+                int newCount = oldCount - 1;
+                map.put(current,newCount);
+            
+            if(newCount < 0){
+                return false;
+            }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }      
+        
+
+    
+}
